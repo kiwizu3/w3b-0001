409eb920ace1ade16d8b70422dd415e839bab790
Kavinda Sumanaweera <kavindasu@gmail.com>
2024-06-21 04:17:46 +0530

    Added a balloon and a bike

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,17 +1,24 @@
 import React from "react";
 import Navbar from "./Navbar";
 import Birds from "./Birds";
+import Balloon from "../assets/images/balloon-1.svg";
+import Bike from "../assets/images/bike-1.svg";
 
 const Header = () => {
   return (
     <>
       <Navbar />
-      <div className="row h-100 header-body">
+      <div className="row h-100 header-body position-relative">
+        
+        <img className="bike position-absolute" src={Bike} />
+        <img className="balloon position-absolute" src={Balloon} />
         <div className="col text-center">
           <Birds />
-          <h1 className="display-3">
-            <span className="fw-semibold">Discover</span> title <br /> goes here{" "}
-            <span className="fw-semibold">like this</span>
+          <h1 className="display-3 px-md-5 px-0">
+            <span className="fw-semibold">Discover</span> the Allure
+            <br />
+            of Sri Lanka's
+            <span className="fw-semibold"> Paradise</span>
           </h1>
           <button className="btn btn-dark rounded-pill btn-sm px-4 py-3">
             Explore Now
