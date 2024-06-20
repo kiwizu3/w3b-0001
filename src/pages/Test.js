d92c0b7fe14fa70a51fcccb938d0dd4dff57d590
Kavinda Sumanaweera <kavindasu@gmail.com>
2024-06-21 03:15:53 +0530

    Test page added

--- src/pages/Test.js
+++ src/pages/Test.js
@@ -1,23 +1,13 @@
 import React from "react";
 
-import Lottie from "react-lottie";
-import animationData from "../lotties/birds.json";
 
 const Test = () => {
-  const defaultOptions = {
-    loop: true,
-    autoplay: true,
-    animationData: animationData,
-    rendererSettings: {
-      preserveAspectRatio: "xMidYMid slice",
-    },
-  };
+  
 
   return (
     <>
       <div className="container-fluid h-100 bg-image">
-        <Lottie options={defaultOptions} height={100} width={100} />
-        <Lottie options={defaultOptions} height={100} width={100} />
+      
       </div>
     </>
   );
